055245aea3f347d8151b3e3935e234eb6b16cdfc
Six087 <112667854+Six087@users.noreply.github.com>
2023-08-28 20:54:10 -0300

    Add files via upload

---
+++ Laboratorio 4 2023/Python/Clase 2/tipo_float.py
@@ -0,0 +1,12 @@
+#Profundizando en el tipo float
+a = 3.0
+print (f"a: {a:.2f}")
+
+#Contrunctor de tipo float -> puede recibir int y str
+a = float(10) # Le pasamos un tipo entero (int)
+a = float("10")
+print (f"a: {a:.2f}")
+
+#Notacion exponencial (valores + o -)
+a = 3e5
+print (f"a: {a}")
