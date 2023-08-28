640e2a842e9270396f20ee83f4e8e987735a2942
Six087 <112667854+Six087@users.noreply.github.com>
2023-08-28 20:53:53 -0300

    Add files via upload

--- Laboratorio 4 2023/Python/Clase 2/Sistemas_numeracion.py
+++ Laboratorio 4 2023/Python/Clase 2/Sistemas_numeracion.py
@@ -16,4 +16,22 @@ print (f"a octal:{a}")
 a=  0xA
 print (f"a hexadecimal:{a}")
 
+#Base decimal
+a = int("23", 10)
+print (f"a = Base decimal: {a}")
 
+#Base binario
+a = int("10111", 2)
+print (f"a = Base binario:{a}")
+
+#Base octal
+a = int("27", 8)
+print (f"a = Base octal:{a}")
+
+#Base hexadecimal
+a = int ("17", 16)
+print (f"a = Base hexadecimal:{a}")
+
+#base 5 su valores son de 0 a 4 
+a = int("344",5)
+print (f"a = Base 5:{a}")
