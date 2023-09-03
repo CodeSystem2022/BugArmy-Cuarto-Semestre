c20c492583efde0465875aa1fea649c7b6307297
Adriel <113069922+SergioMezzabotta@users.noreply.github.com>
2023-09-03 00:16:52 -0300

    clase 3

---
+++ Laboratorio 4 2023/Java Script/Clase 1/e-commerce2022/client/js/products.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Zapatillas",
+        price: 17969,
+        quanty: 45,
+        img: "/client/media/zapatillas.jpg"
+    },
+    {
+        id: 2,
+        productName: "Hacha",
+        price: 17958,
+        quanty: 12,
+        img: "/client/media/hacha.jpg"
+    },
+    {
+        id: 3,
+        productName: "Neumatico Dunlop",
+        price: 111876,
+        quanty: 5,
+        img: "/client/media/rueda.jpg"
+    },
+    {
+        id: 4,
+        productName: "Control Joystick PS5",
+        price: 63999,
+        quanty: 43,
+        img: "/client/media/control.jpg"
+    },
+    {
+        id: 5,
+        productName: "Mochila",
+        price: 14990,
+        quanty: 10,
+        img: "/client/media/mochila.jpg"
+    },
+    {
+        id: 6,
+        productName: "Celular Chamsun 22",
+        price: 142980,
+        quanty: 4,
+        img: "/client/media/celular.jpg"
+    },
+    {
+        id: 7,
+        productName: "Cartera",
+        price: 11725,
+        quanty: 8,
+        img: "/client/media/cartera.jpg"
+    },
+    {
+        id: 8,
+        productName: "Bolso",
+        price: 9500,
+        quanty: 13,
+        img: "/client/media/bolso.jpg"
+    },
+];
