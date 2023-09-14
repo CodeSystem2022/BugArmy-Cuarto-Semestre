b64ca97b1fcc22801518dd772fdcb0eaa2bfa31f
Adriel <113069922+SergioMezzabotta@users.noreply.github.com>
2023-09-14 20:55:19 -0300

    E-COMMERCE

---
+++ E-COMMERCE/client/js/products.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Zapatillas",
+        price: 17969,
+        quanty: 1,
+        img: "media/zapatillas.jpg"
+    },
+    {
+        id: 2,
+        productName: "Hacha",
+        price: 17958,
+        quanty: 1,
+        img: "media/hacha.jpg"
+    },
+    {
+        id: 3,
+        productName: "Neumatico Dunlop",
+        price: 111876,
+        quanty: 1,
+        img: "media/rueda.jpg"
+    },
+    {
+        id: 4,
+        productName: "Control Joystick PS5",
+        price: 63999,
+        quanty: 1,
+        img: "media/control.jpg"
+    },
+    {
+        id: 5,
+        productName: "Mochila",
+        price: 14990,
+        quanty: 1,
+        img: "media/mochila.jpg"
+    },
+    {
+        id: 6,
+        productName: "Celular Chamsun 22",
+        price: 142980,
+        quanty: 1,
+        img: "media/celular.jpg"
+    },
+    {
+        id: 7,
+        productName: "Cartera",
+        price: 11725,
+        quanty: 1,
+        img: "media/cartera.jpg"
+    },
+    {
+        id: 8,
+        productName: "Bolso",
+        price: 9500,
+        quanty: 1,
+        img: "media/bolso.jpg"
+    },
+];
