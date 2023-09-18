2a49cb1221e281a0bb36ead540bec0ee0cdfd39e
Six087 <112667854+Six087@users.noreply.github.com>
2023-09-18 20:14:36 -0300

    Clase 5 de python

---
+++ Laboratorio 4 2023/Python/Clase 2/mi_clase.py
@@ -0,0 +1,22 @@
+
+
+class MiClase:
+    '''
+    Este es un ejemplo de documentacion
+    de la clase.
+    es solo para saber que aqui esta Miclase
+    '''
+    def __init__(self):
+        '''
+        Aqui esta el metodo dunder init
+        este metodo es el de inicio
+        para est a clase
+        '''
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mi que recibe parámetros
+        Detectó los parámetros automaticamente
+        :param param1: Este es el parámetro número 1
+        :param param2: Este es el parámetro número 2
+        :return: Este es el valor de retorno
+        '''
