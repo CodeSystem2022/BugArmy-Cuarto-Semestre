b2f6c48ec34e60e6f50fe1b26eb95588dfaf5494
Luciano <91286415+BugaToro@users.noreply.github.com>
2023-10-18 22:43:46 -0300

    Clase 08 - Callbacks
    Cómo gestionar algo asíncrono

---
+++ Metodologia de Sistemas 2023/02-FundamentoS_node/async/callback.js
@@ -0,0 +1,12 @@
+
+function soyAsincrona(){
+    setTimeout( function (miCallback) {
+        console.log("Hola, soy una función asíncrona ");
+        miCallback();
+    }, 1000);    
+}
+
+console.log("Iniciando el proceso...");
+soyAsincrona(function() {
+    console.log("Terminando el proceso...");
+});
